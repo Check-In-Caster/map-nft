dcc08b0d72641fb111a8c1dd7c09aa78f8a283bb
PJijin <pjijin1@gmail.com>
2024-08-09 14:43:22 +0530

    fix: updated mainnet contract env

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ export const DOMAIN =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : process.env.NEXT_PUBLIC_DOMAIN
-    ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
-    : "https://maps.checkin.gg";
+      ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
+      : "https://maps.checkin.gg";
 
 export const is_dev =
   process.env.NEXT_PUBLIC_IS_DEVELOPMENT === "true" ? true : false;
@@ -12,7 +12,7 @@ export const CHAIN_ID = is_dev ? 84532 : 8453;
 
 export const CONTRACT_ADDRESS = is_dev
   ? "0xe5fa8c8f7497b94a47ed42778dc508fc0ec3bb79"
-  : (process.env.MAINNET_CONTRACT_ADDRESS! as "0x");
+  : (process.env.NEXT_PUBLIC_MAINNET_CONTRACT_ADDRESS! as "0x");
 
 export const RPC_PROVIDER = is_dev
   ? "https://sepolia.base.org"
